ea5ddaa717036daffb42006f86eb7fc0dae84e87
Paweł Sroka <P.sroka@onet.eu>
2021-11-02 20:56:20 +0100

    base

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once('resources/views/header.php');
+
+require_once('resources/views/footer.php') ;
+
---
+++ resources/views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Page Title</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="../css/mdb.min.css" type="text/css">
+<script src="../js/mdb.min.js"></script>
+<body>
+
+<nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand">Navbar</a>
+        <form class="d-flex input-group w-auto">
+            <input
+                    type="search"
+                    class="form-control rounded"
+                    placeholder="Search"
+                    aria-label="Search"
+                    aria-describedby="search-addon"
+            />
+            <span class="input-group-text border-0" id="search-addon">
+        <i class="fas fa-search"></i>
+      </span>
+        </form>
+    </div>
+</nav>
+
+</body>
+</html>
